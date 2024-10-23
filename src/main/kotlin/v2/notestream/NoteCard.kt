a723badb5b2e7afbf3dad5490292d3b67f0de52c
Marcin Radoszewski <moriturius@gmail.com>
2024-10-23 19:08:01 +0200

    Always scroll cursor in the editor into view

--- src/main/kotlin/v2/notestream/NoteCard.kt
+++ src/main/kotlin/v2/notestream/NoteCard.kt
@@ -7,22 +7,31 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.runtime.*
 import editor.editorFont
 import kotlinx.coroutines.flow.map
 import v2.Buffer
@@ -43,16 +52,20 @@ data class NoteCardState(
 @Composable
 fun NoteCard(state: NoteCardState,
              modifier: Modifier = Modifier,
+             textFieldModifier: Modifier = Modifier,
+             active: Boolean = false,
              onChange: (NoteCardState) -> Unit = {},
-             onClose: () -> Unit = {}
+             onClose: () -> Unit = {},
+             onTextLayout: (TextLayoutResult) -> Unit = {},
 ) {
+    val focusRequester = remember {  FocusRequester() }
     val content by state.buffer.content.map {
         TextFieldValue(it, state.selection)
     }.collectAsState(state.textFieldValue)
 
     Card(modifier) {
         Column(Modifier.padding(8.dp)) {
-            Row(Modifier.padding(vertical = 10.dp).fillMaxWidth()) {
+            Row(Modifier.padding(vertical = 10.dp).fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
                 Text(
                     state.buffer.name.value,
                     style = MaterialTheme.typography.h6)
@@ -66,13 +79,20 @@ fun NoteCard(state: NoteCardState,
             }
             BasicTextField(
                 content,
-                modifier = Modifier.fillMaxWidth(),
+                modifier = textFieldModifier.fillMaxWidth().focusRequester(focusRequester),
                 onValueChange = { value -> onChange(state.update(value)) },
+                onTextLayout = onTextLayout,
                 textStyle = TextStyle(
                     fontFamily = editorFont,
                     fontSize = 16.sp
                 ),
             )
+        }
+    }
+
+    LaunchedEffect(active) {
+        if (active) {
+            focusRequester.requestFocus()
         }
     }
 }
--- src/main/kotlin/v2/notestream/NoteStream.kt
+++ src/main/kotlin/v2/notestream/NoteStream.kt
@@ -1,6 +1,7 @@
 package v2.notestream
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,14 +10,18 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 fun <E> Iterable<E>.updated(index: Int, elem: E) = mapIndexed { i, existing ->  if (i == index) elem else existing }
 
@@ -56,6 +61,8 @@ fun NoteStream(state: NoteStreamState,
                lazyListState: LazyListState = LazyListState(),
                outlineNote: Int = 0
                ) {
+    val coroutineScope = rememberCoroutineScope()
+
     if (state.cards.isNotEmpty()) {
         LazyColumn(
             modifier,
@@ -63,8 +70,49 @@ fun NoteStream(state: NoteStreamState,
             verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
             itemsIndexed(state.cards) { idx, card ->
+                var textLayout: TextLayoutResult? by remember { mutableStateOf(null) }
+                var textFieldPosition by remember { mutableStateOf(0f) }
+
+                fun scrollIfCursorOutOfView(cursorOffset: Int) {
+                    val item = lazyListState.layoutInfo.visibleItemsInfo.find { it.index == idx }
+                    val layout = textLayout
+                    if (layout != null && item != null && !lazyListState.isScrollInProgress) {
+                        val cursor = runCatching { layout.getCursorRect(cursorOffset) }
+                            .getOrElse { layout.getCursorRect(state.cards[idx].selection.start) }
+                        // I'm subtracting/adding cursor.height * 2 to make some space around the cursor after scroll
+                        val cursorTop = (cursor.top + item.offset + textFieldPosition - cursor.height*2)
+                        val cursorBottom = (cursor.bottom + item.offset + textFieldPosition + cursor.height*2)
+
+                        val cursorDistanceFromViewportStart =
+                            lazyListState.layoutInfo.viewportStartOffset - cursorTop
+                        val cursorDistanceFromViewportEnd =
+                            lazyListState.layoutInfo.viewportEndOffset - cursorBottom
+
+                        var scrollOffset = 0f
+                        if (cursorDistanceFromViewportStart > 0) {
+                            // should scoll up
+                            println("scrolling up by $cursorDistanceFromViewportStart")
+                            scrollOffset = -cursorDistanceFromViewportStart
+                        } else if (cursorDistanceFromViewportEnd < 0) {
+                            // should scroll down
+                            println("scrolling down by $cursorDistanceFromViewportEnd")
+                            scrollOffset = -cursorDistanceFromViewportEnd
+                        } else {
+                            scrollOffset = 0f
+                        }
+                        coroutineScope.launch {
+                            lazyListState.animateScrollBy(scrollOffset)
+                        }
+                    }
+                }
+
                 NoteCard(card,
-                    onChange = { onUpdate(state.updateCard(idx, it)) },
+                    active = outlineNote == idx,
+                    onChange = {
+                        val position = it.selection.start.coerceAtMost(state.cards[idx].textFieldValue.text.length)
+                        onUpdate(state.updateCard(idx, it))
+                        scrollIfCursorOutOfView(position)
+                    },
                     onClose = { onUpdate(state.closeCardAt(idx)) },
                     modifier = Modifier.onFocusChanged {
                         if (it.hasFocus) {
@@ -77,6 +125,13 @@ fun NoteStream(state: NoteStreamState,
                         } else {
                             it
                         }
+                    },
+                    textFieldModifier = Modifier.onGloballyPositioned {
+                        textFieldPosition = it.positionInParent().y
+                    },
+                    onTextLayout = { layout ->
+                        textLayout = layout
+                        scrollIfCursorOutOfView(state.cards[idx].selection.start)
                     }
                 )
             }
